26b30ec85ef20d13d63ec25144d6ce5ae381158b
LUCKYprog <lgunda@uncc.edu>
2024-09-17 19:24:42 +0000

    "Completed Movie Script Analysis Assignment"

--- src/main/java/com/movie/script/analysis/DialogueLengthReducer.java
+++ src/main/java/com/movie/script/analysis/DialogueLengthReducer.java
@@ -10,6 +10,14 @@ public class DialogueLengthReducer extends Reducer<Text, IntWritable, Text, IntW
 
     @Override
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int totalLength = 0;
 
+        // Sum all dialogue lengths for the given character
+        for (IntWritable length : values) {
+            totalLength += length.get();
+        }
+
+        // Emit the character and total dialogue length
+        context.write(key, new IntWritable(totalLength));
     }
 }
